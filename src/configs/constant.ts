5b478b66730d9b679fe0013d4efed93cfc7926fb
Binh <binh.nguyen@blockdevs.net>
2021-09-20 13:49:49 +0700

    feat: change provider url

--- src/configs/constant.ts
+++ src/configs/constant.ts
@@ -1,7 +1,6 @@
 export const ETH_ADDRESS = '0x0000000000000000000000000000000000000000';
 
 export const WS_PROVIDER = 'wss://westend-rpc.polkadot.io/';
-// export const WS_PROVIDER = 'wss://mandala6.laminar.codes/'
 
 export const FAUCET =
   'https://app.element.io/#/room/#westend_faucet:matrix.org';
